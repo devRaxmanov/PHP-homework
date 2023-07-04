08121dc5aab032228e13cc14218c54b681fd5b85
devRaxmanov <raxmanovjorabek5@gmail.com>
2023-07-04 10:13:05 +0500

    PHP poject

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles/main.css">
+    <title>Function-Array</title>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="container">
+            <h1 class="title mb-5">Function homework</h1>
+            <div class="row cards">
+                <div class="col-md-4">
+                    <div class="card exampleCard">
+                        <div class="card-header">1-misol</div>
+                        <div class="card-body">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque, totam!
+                        </div>
+                        <div class="card-footer">
+                            Natija:
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <script src="script/script.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+</body>
+</html>
